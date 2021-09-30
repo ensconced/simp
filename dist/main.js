749aec0def40d2082e40bff6907d8f4ece1fb8f2
Joe Barnett <joebarnett876@gmail.com>
2021-09-30 22:10:59 +0100

    actuall build the thing

--- dist/main.js
+++ dist/main.js
@@ -56,21 +56,11 @@ const builder = {
   },
 };
 
-function getMode(argv) {
-  if (argv.mode) {
-    const modes = argv.config.modes;
-    if (modes[argv.mode]) {
-      return modes[argv.mode];
-    }
-  }
-  return undefined;
-}
-
 function handler(argv) {
   const { files } = argv;
 
   // TODO - currently we just use the first file...
-  buildFile(files[0], getMode(argv));
+  buildFile(files[0], argv);
 }
 
 module.exports = { command, describe, builder, handler };
@@ -88,13 +78,47 @@ const exec = util.promisify(__webpack_require__(81).exec);
 
 const addDependencies = __webpack_require__(902);
 
-async function buildFile(file, modeOptions) {
+function getFlagsForMode(mode, config) {
+  const modeObject = config.modes[mode];
+  if (modeObject) {
+    if (modeObject.extends) {
+      // TODO - infinite loop for extends cycles - don't do that!
+      const superFlags = getFlagsForMode(modeObject.extends, config);
+      const subFlags = modeObject.flags || "";
+      return superFlags + " " + subFlags;
+    }
+    return modeObject.flags || "";
+  }
+  return "";
+}
+
+function getFlags(argv) {
+  if (argv.config.modes) {
+    if (argv.mode) {
+      return getFlagsForMode(argv.mode, argv.config);
+    } else if (argv.config.modes.default) {
+      return getFlagsForMode("default", argv.config);
+    }
+  }
+  return "";
+}
+// const mode = modes[argv.mode];
+// if (argv.mode) {
+//   const modes = argv.config.modes;
+//   if (mode) {
+//     const flags = mode.flags;
+//     console.log(flags);
+//   }
+// }
+// return "";
+
+async function buildFile(file, argv) {
   const fileName = await fs.promises.realpath(file);
   const sourceFiles = new Set();
   await addDependencies(fileName, sourceFiles);
   const sourceFileArgs = [...sourceFiles].map((file) => `"${file}"`).join(" ");
   const outFile = fileName.replace(/\.c$/, "");
-  const flags = (modeOptions && modeOptions.flags) || "";
+  const flags = getFlags(argv);
   const commandParts = ["clang", flags, sourceFileArgs, "-o", `"${outFile}"`];
   const command = commandParts.map((part) => part.trim()).join(" ");
   // TODO - proper logging
